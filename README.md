032125062d1250fdd6960f447b5510904fe5786a
wdxtub <wdxtub@qq.com>
2017-03-31 11:57:34 +0800

    minor update

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Patriots
 
+一个集机器学习、数据挖掘等各类人工智能实践的 web 管理系统
+
 Data Mining & Machine Learning / Deep Learning Platform with Web UI
 
+## 技术栈
+
++ 后端
+    + beego(go 1.8)
+    + python 2.7
+    + tensorflow
+    + keras
++ 前端
+    + vue.js
+    + element.ui
 
 ## 致谢
 
--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
 	beego.Run()
+	beego.Trace("Patriots are ready for your command, SIR.")
 }
 
