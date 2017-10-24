1d618762b948e526fb974dfbf1cf598cbe35d71a
wdxtub <dacrocodilee@gmail.com>
2017-10-24 19:59:19 +0800

    minor change

---
+++ archive/linux_script/centos-docker-install.sh
@@ -0,0 +1,3 @@
+# CentOS docker 安装脚本
+# https://docs.docker.com/engine/installation/linux/docker-ce/centos/
+
---
+++ routers/commentsRouter____patriots_controllers.go
@@ -0,0 +1,114 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["patriots/controllers:AlgorithmController"] = append(beego.GlobalControllerRouter["patriots/controllers:AlgorithmController"],
+		beego.ControllerComments{
+			Method: "CreateAlgorithm",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:AlgorithmController"] = append(beego.GlobalControllerRouter["patriots/controllers:AlgorithmController"],
+		beego.ControllerComments{
+			Method: "GetAlgorithm",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:AlgorithmController"] = append(beego.GlobalControllerRouter["patriots/controllers:AlgorithmController"],
+		beego.ControllerComments{
+			Method: "GetAlgorithmList",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:AlgorithmController"] = append(beego.GlobalControllerRouter["patriots/controllers:AlgorithmController"],
+		beego.ControllerComments{
+			Method: "DeleteAlgorithm",
+			Router: `/:id/delete`,
+			AllowHTTPMethods: []string{"post"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:StatusController"] = append(beego.GlobalControllerRouter["patriots/controllers:StatusController"],
+		beego.ControllerComments{
+			Method: "Login",
+			Router: `/login`,
+			AllowHTTPMethods: []string{"post"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:StatusController"] = append(beego.GlobalControllerRouter["patriots/controllers:StatusController"],
+		beego.ControllerComments{
+			Method: "Logout",
+			Router: `/logout`,
+			AllowHTTPMethods: []string{"post"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:StatusController"] = append(beego.GlobalControllerRouter["patriots/controllers:StatusController"],
+		beego.ControllerComments{
+			Method: "Status",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:TestsetController"] = append(beego.GlobalControllerRouter["patriots/controllers:TestsetController"],
+		beego.ControllerComments{
+			Method: "CreateTestset",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:TestsetController"] = append(beego.GlobalControllerRouter["patriots/controllers:TestsetController"],
+		beego.ControllerComments{
+			Method: "GetTestset",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:TestsetController"] = append(beego.GlobalControllerRouter["patriots/controllers:TestsetController"],
+		beego.ControllerComments{
+			Method: "GetTestsetList",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:TestsetController"] = append(beego.GlobalControllerRouter["patriots/controllers:TestsetController"],
+		beego.ControllerComments{
+			Method: "DeleteTestset",
+			Router: `/:id/delete`,
+			AllowHTTPMethods: []string{"post"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:TrainsetController"] = append(beego.GlobalControllerRouter["patriots/controllers:TrainsetController"],
+		beego.ControllerComments{
+			Method: "CreateTrainset",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:TrainsetController"] = append(beego.GlobalControllerRouter["patriots/controllers:TrainsetController"],
+		beego.ControllerComments{
+			Method: "GetTrainset",
+			Router: `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:TrainsetController"] = append(beego.GlobalControllerRouter["patriots/controllers:TrainsetController"],
+		beego.ControllerComments{
+			Method: "GetTrainsetList",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			Params: nil})
+
+	beego.GlobalControllerRouter["patriots/controllers:TrainsetController"] = append(beego.GlobalControllerRouter["patriots/controllers:TrainsetController"],
+		beego.ControllerComments{
+			Method: "DeleteTrainset",
+			Router: `/:id/delete`,
+			AllowHTTPMethods: []string{"post"},
+			Params: nil})
+
+}
