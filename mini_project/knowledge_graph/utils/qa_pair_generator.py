7aef65dd621070b4a7b99219bc0a941c625292fd
wdxtub <dacrocodilee@gmail.com>
2017-10-26 21:53:09 +0800

    完成语料生成

--- mini_project/knowledge_graph/utils/qa_pair_generator.py
+++ mini_project/knowledge_graph/utils/qa_pair_generator.py
@@ -1,6 +1,7 @@
 #-*- coding:utf8 -*-
 #!/usr/bin/python2.7
 
+from jinja2 import Template
 from optparse import OptionParser
 
 import os, sys
@@ -30,33 +31,6 @@ def CheckOption(option, option_str):
         return 1
     return 0
 
-# 根据传入的字典和模板来生成字符串
-def Render(template, para_dict):
-    # 先根据 \t 进行分隔
-    print template
-    print para_dict
-    result = []
-    tokens = template.split('\t')
-    for token in tokens:
-        # 从第 0 个开始搜索
-        start = token.find('{{', 0)
-        end = token.find('}}', 0)
-        if start == -1 and end == -1: # 如果俩都没有，直接加入数组，然后跳过
-            result.append(token)
-            continue
-        # TODO 这里需要用 while，来确保能够完全替换
-        if start != -1 and end != -1: # 如果都能找到，则进行替换后加入数组
-            # 先找到字典的 key
-            key = token[start+2:end]
-            print key
-            continue
-        # 到这里的话，说明
-        
-
-
-    return "lalala"
-
-
 # python qa_pair_generator.py --triad_path=product_graph --template=qa_pair.template
 # python qa_pair_generator.py --triad_path=test --template=qa_pair.template
 
@@ -78,6 +52,7 @@ if __name__ == "__main__":
 
     print "Step 2 Load Triad Data from %s" % options.triad_path
 
+    result = []
     file_list= [i for i in os.listdir(options.triad_path)]
     for filename in file_list:
         path = options.triad_path + "/" + filename
@@ -90,10 +65,14 @@ if __name__ == "__main__":
             para_dict = {}
             para_dict['subject_entity'] = arr[0]
             para_dict['relationship'] = tokens[4][:-1] # 去掉最后的回车
-            para_dict['object_entity'] = tokens[3]
+            para_dict['object_entity'] = tokens[2]
             # 根据模板进行句子生成
-            for template in template_list:
-                print Render(template, para_dict)
+            for t in template_list:
+                template = Template(t)
+                result.append("\t".join(template.render(para_dict).split("##")))
+
+    for r in result:
+        print r
 
 
     
